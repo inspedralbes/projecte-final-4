41133f29ee89691d11d54af87bf4b9969e03c222
a20alvlozhue <a20alvlozhue@inspedralbes.cat>
2022-05-02 10:34:42 +0200

    Lletres

---
+++ web/joc/src/components/lletres.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <h1>Lletres</h1>
+    <div class="row">
+      <div class="col m2">R</div>
+      <div class="col m2">F</div>
+      <div class="col m2">E</div>
+    </div>
+    <div class="row">
+      <div class="col m2">N</div>
+      <div class="col m2">A</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+</style>
--- web/joc/src/views/HomeView.vue
+++ web/joc/src/views/HomeView.vue
@@ -1,11 +1,19 @@
-<script setup>
+<script>
 import 'materialize-css'
+import lletres from "../components/lletres.vue";
 
+export default{
+  components:{
+        lletres,
+
+    },
+}
 </script>
 
 <template>
   <main>
     <h1>Joc</h1>
+    <lletres/>
     <h2>1 Paraula</h2>
      <div class="row">
       <div class="col m2"><input/></div>
