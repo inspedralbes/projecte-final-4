dca738f891ee92466d7db17d9ef81034e6fb35a0
a20alvlozhue <a20alvlozhue@inspedralbes.cat>
2022-05-02 09:38:44 +0200

    vue2

--- web/joc/src/views/HomeView.vue
+++ web/joc/src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Joc</h1>
   </main>
 </template>
+
+<style>
+
+
+</style>
