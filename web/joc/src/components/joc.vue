8e6dbd63c5a2e0254b9fc0fb9a027031c3923a20
a20alvlozhue <a20alvlozhue@inspedralbes.cat>
2022-05-03 12:02:26 +0200

    Revert "Update joc.vue"
    This reverts commit d1aad48699cd8e05bf5224cc14f5a74f39df835f.

--- web/joc/src/components/joc.vue
+++ web/joc/src/components/joc.vue
@@ -43,37 +43,37 @@
 <<<<<<< Updated upstream
   <div id="keyboard-cont">
         <div class="first-row">
-            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="1">Q</button>
+            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="q">q</button>
             <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="w">w</button>
-            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="E">E</button>
-            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="R">R</button>
-            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="T">T</button>
-            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="Y">Y</button>
-            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="U">U</button>
-            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="I">I</button>
-            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="O">O</button>
-            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="P">P</button>
+            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="e">e</button>
+            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="r">r</button>
+            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="t">t</button>
+            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="y">y</button>
+            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="u">u</button>
+            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="i">i</button>
+            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="o">o</button>
+            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="p">p</button>
         </div>
         <div class="second-row">
-            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="A">A</button>
-            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="S">S</button>
-            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="D">D</button>
-            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="F">F</button>
-            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="G">G</button>
-            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="H">H</button>
-            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="J">J</button>
-            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="K">K</button>
-            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="L">L</button>
+            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="a">a</button>
+            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="s">s</button>
+            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="d">d</button>
+            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="f">f</button>
+            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="g">g</button>
+            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="h">h</button>
+            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="j">j</button>
+            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="k">k</button>
+            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="l">l</button>
         </div>
         <div class="third-row">
             <button class="waves-effect waves-light btn">Del</button>
-            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="Z">Z</button>
-            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="X">X</button>
-            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="C">C</button>
-            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="V">V</button>
-            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="B">B</button>
-            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="N">N</button>
-            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="M">M</button>
+            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="z">z</button>
+            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="x">x</button>
+            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="c">c</button>
+            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="v">v</button>
+            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="b">b</button>
+            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="n">n</button>
+            <button v-on:click="apretar($event)" class="waves-effect waves-light btn" name="m">m</button>
             <button class="waves-effect waves-light btn">Enter</button>
         </div>
 =======
