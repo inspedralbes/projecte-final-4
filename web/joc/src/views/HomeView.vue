d7b54679a37cda5f7c912163029b61c3dfbfd695
a18polpuipui <a18polpuipui@inspedralbes.cat>
2022-05-05 09:40:01 +0200

    header

--- web/joc/src/views/HomeView.vue
+++ web/joc/src/views/HomeView.vue
@@ -14,7 +14,18 @@ export default {
 </script>
 
 <template>
-  <main>
+<div>
+  <nav class="#66bb6a green lighten-1">
+    <div class="nav-wrapper">
+      <img src="src/components/img/logo2.png" alt="Girl in a jacket" width="100" height="70">
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="#">Estadistiques</a></li>
+        <li><a href="#">Ajustes</a></li>
+        <li><a href="#">Puntuacións</a></li>
+      </ul>
+    </div>
+  </nav>
+    <main>
     <div class="row">
     
     <div class="col s12 m4 l2"><p></p></div>
@@ -31,6 +42,8 @@ export default {
   </div>
    
   </main>
+</div>
+
 </template>
 
 <style>
