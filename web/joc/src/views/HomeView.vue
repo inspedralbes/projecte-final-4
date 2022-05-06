c90835a655ebe952a776c2f53155a5be0ad53637
a18polpuipui <a18polpuipui@inspedralbes.cat>
2022-05-06 12:34:22 +0200

    npm run build

--- web/joc/src/views/HomeView.vue
+++ web/joc/src/views/HomeView.vue
@@ -17,7 +17,7 @@ export default {
 <div>
   <nav class="#66bb6a green lighten-1">
     <div class="nav-wrapper">
-      <img src="src/components/img/logo2.png" alt="Girl in a jacket" width="100" height="70">
+      <img src="./img/logo2.png" alt="Girl in a jacket" width="100" height="70">
       <ul id="nav-mobile" class="right hide-on-med-and-down">
         <li><a href="#">Estadistiques</a></li>
         <li><a href="#">Ajustes</a></li>
