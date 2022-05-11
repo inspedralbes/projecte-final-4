ed6a00555d25c10ed3635135bfacddab3fc2d835
a18polpuipui <a18polpuipui@inspedralbes.cat>
2022-05-11 09:14:46 +0200

    boton logo href="/"

--- web/joc/src/components/Header.vue
+++ web/joc/src/components/Header.vue
@@ -1,7 +1,9 @@
 <template>
   <nav class="#66bb6a green lighten-1">
     <div class="nav-wrapper">
-      <img src="./img/logo2.png" alt="Girl in a jacket" width="100" height="70">
+        <a href="/">
+         <img alt="Qries" src="./img/logo2.png" width="100" height="70">
+      </a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
         <li><RouterLink :to="'/puntuacions'"  >Puntuacions</RouterLink></li>
         <li><a href="#">Ajustes</a></li>
