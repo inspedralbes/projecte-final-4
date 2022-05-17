c00cb424c58ce951d8e27c41ca874ce65185205e
a18polpuipui <a18polpuipui@inspedralbes.cat>
2022-05-17 08:59:37 +0200

    modal

--- web/joc/src/components/Header.vue
+++ web/joc/src/components/Header.vue
@@ -8,14 +8,34 @@
         <li><RouterLink :to="'/puntuacions'"  >Puntuacions</RouterLink></li>
         <li><a href="#">Ajustes</a></li>
         <li><a href="#">Estadistiques</a></li>
+        <li><a class="waves-effect waves-light btn modal-trigger" href="#modal1">Modal</a></li>
       </ul>
     </div>
+     <div class="container">
+            <!-- Modal Structure -->
+            <div id="modal1" class="modal">
+                <div class="modal-content">
+                    <h4>Modal Header</h4>
+                    <p>A bunch of text</p>
+                </div>
+                <div class="modal-footer">
+                    <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+                </div>
+            </div>
+       </div>
   </nav>
 </template>
 
 <script>
-export default {
 
+export default {
+mounted() {
+     document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+  });
+
+},
 }
 </script>
 
