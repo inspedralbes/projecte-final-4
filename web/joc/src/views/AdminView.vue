b5eee44694987cd91d4867b9d5a210f63e6ae41d
a20alvlozhue <a20alvlozhue@inspedralbes.cat>
2022-05-22 17:31:11 +0200

    css admin

--- web/joc/src/views/AdminView.vue
+++ web/joc/src/views/AdminView.vue
@@ -1,64 +1,58 @@
 <template>
-  <div class="body">
-    <a class="waves-effect waves-light btn-large" href="/">HOME</a>
-    <br />
-    <div class="row">
-      <div class="col s12">
-        <input
-          placeholder="Palabra"
-          id="afegirPalabra"
-          type="text"
-          class="validate"
-        />
+  <div>
+    <Header />
+    <br>
+    <div class="row center-align box">
+
+      <div class="col s6 ">
+        <a class="waves-effect waves-light btn-large" href="/">HOME</a>
+        <br>
+        <a class="waves-effect waves-light btn-large" v-on:click="mostrarPalabras()">Ver Palabras</a>
+
       </div>
+      <div class="col s6 ">
+        <input placeholder="Palabra" id="afegirPalabra" type="text" class="validate" />
+        <input placeholder="Definicion" id="afegirDefinicion" type="text" class="validate" />
+        <a class="waves-effect waves-light btn-large" v-on:click="añadirPalabra()">Añadir Palabra</a>
+
+      </div>
+
     </div>
-    <div class="row">
-      <div class="col s12">
-        <input
-          placeholder="Definicion"
-          id="afegirDefinicion"
-          type="text"
-          class="validate"
-        />
-      </div>
-    </div>
-    <div class="row">
-      <div class="col s6">
-        <a
-          class="waves-effect waves-light btn-large"
-          v-on:click="añadirPalabra()"
-          >Añadir Palabra</a
-        >
-      </div>
-      <div class="col s6">
-        <a
-          class="waves-effect waves-light btn-large"
-          v-on:click="mostrarPalabras()"
-          >Ver Palabras</a
-        >
-      </div>
+    <br>
+    <div class="row box">
       <table class="striped" id="todotable">
-        <tr>
-                <th>Palabra</th>
-                <th>Definición</th>
-                <th></th>
-                </tr>
-                <tr v-for="pal in listaPalabras">
-                    <td>{{pal.palabra}}</td>
-                    <td>{{pal.definicion}}</td>
-                    <td><button class="btn-floating btn-large waves-effect waves-light red" v-bind:id="pal._id" @click="deletePalabra(pal._id)"><i class="material-icons">delete_forever</i></button></td>
-              </tr>
+        <thead>
+          <tr>
+            <th>Palabra</th>
+            <th>Definición</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="pal in listaPalabras">
+            <td>{{ pal.palabra }}</td>
+            <td>{{ pal.definicion }}</td>
+            <td><button class="btn-floating btn-large waves-effect waves-light red" v-bind:id="pal._id"
+                @click="deletePalabra(pal._id)"><i class="material-icons">delete_forever</i></button></td>
+          </tr>
+        </tbody>
       </table>
     </div>
   </div>
 </template>
 
 <script>
+import Header from '../components/Header.vue';
+
 export default {
+  components: {
+    Header,
+
+  },
   data() {
     return {
       palabraView: [],
-      listaPalabras:[],
+      listaPalabras: [],
     };
   },
   methods: {
@@ -96,30 +90,31 @@ export default {
         .then((response) => response.json())
         .then((data) => {
           console.log(data);
-          this.listaPalabras=data;
-          console.log("lenght", data.length);})
-        
-    
+          this.listaPalabras = data;
+          console.log("lenght", data.length);
+        })
+
+
     },
     deletePalabra(id) {
       console.log("id: ", id);
-        fetch("http://paraudl.back.alumnes.inspedralbes.cat:7099/api/todos/" + id, {
-          method: "DELETE",
-           headers: {
+      fetch("http://paraudl.back.alumnes.inspedralbes.cat:7099/api/todos/" + id, {
+        method: "DELETE",
+        headers: {
           "Content-Type": "application/json",
         },
-        })
-          .then((res) => res.text())
-          .then((res) => console.log(res));
-          this.mostrarPalabras();
-      },
+      })
+        .then((res) => res.text())
+        .then((res) => console.log(res));
+      this.mostrarPalabras();
     },
-  }
+  },
+}
 
 </script>
 
 <style>
-.body{
-  padding: 15px;
+.box {
+  padding: 10px;
 }
 </style>
